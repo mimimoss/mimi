9779c42eaeee26b2f767a7c6c5da40ac205f53e5
mimimoss <92761200+mimimoss@users.noreply.github.com>
2024-04-24 11:40:18 -0400

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,17 @@
-const bannerText = document.getElementById("bannerText"); 
-  
-animate(bannerText); 
-  
-function animate(element) { 
-    let elementWidth = element.offsetWidth; 
-    let parentWidth = element.parentElement.offsetWidth; 
-    let flag = 0; 
-  
-    setInterval(() => { 
-        element.style.marginLeft = --flag + "px"; 
-  
-        if (elementWidth == -flag) { 
-            flag = parentWidth; 
-        } 
-    }, 10); 
-} 
+const bannerText = document.getElementById("bannerText");
+
+animate(bannerText);
+
+function animate(element) {
+  let elementWidth = element.offsetWidth;
+  let parentWidth = element.parentElement.offsetWidth;
+  let flag = 0;
+
+  setInterval(() => {
+    if (flag >= parentWidth) {
+      flag = -elementWidth;
+    }
+    element.style.marginLeft = flag + "px";
+    flag++;
+  }, 10);
+}
