19bdcc4869c963b718f53457af6d881d225bb0c0
mimimoss <92761200+mimimoss@users.noreply.github.com>
2024-04-24 11:23:08 -0400

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,26 +1,17 @@
-const bannerText = document.getElementById("bannerText");
-animate(bannerText);
-
-function animate(element) {
-    let cloneWidth = element.offsetWidth;
-    let parentWidth = element.parentElement.offsetWidth;
-    let flag = 0;
-
-    while (flag < parentWidth + cloneWidth) {
-        let clone = element.cloneNode(true);
-        element.parentElement.appendChild(clone);
-        clone.style.left = flag + "px";
-        flag += cloneWidth + 10; // 10px gap
-    }
-
-    setInterval(() => {
-        let clones = document.querySelectorAll('.text');
-        clones.forEach(clone => {
-            let left = parseInt(clone.style.left);
-            clone.style.left = left - 1 + "px";
-            if (left <= -cloneWidth) {
-                clone.style.left = parentWidth + "px";
-            }
-        });
-    }, 10);
-}
+const bannerText = document.getElementById("bannerText"); 
+  
+animate(bannerText); 
+  
+function animate(element) { 
+    let elementWidth = element.offsetWidth; 
+    let parentWidth = element.parentElement.offsetWidth; 
+    let flag = 0; 
+  
+    setInterval(() => { 
+        element.style.marginLeft = --flag + "px"; 
+  
+        if (elementWidth == -flag) { 
+            flag = parentWidth; 
+        } 
+    }, 10); 
+} 
