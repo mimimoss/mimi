0df513e29fcb82a1356223c2d3b5e59308f89dea
mimimoss <92761200+mimimoss@users.noreply.github.com>
2024-05-01 21:09:57 -0400

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,32 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const bannerText = document.getElementById("bannerText");
-  if (bannerText) {
-    animate(bannerText);
-  } else {
-    console.error("Element with ID 'bannerText' not found");
-  }
-});
+const accordion = document.getElementsByClassName('list-item');
 
-const bannerText = document.getElementById("bannerText");
-
-animate(bannerText);
-
-function animate(element) {
-
-  if (!element) {
-  console.error("Element not found");
-  return;
-  }
-  
-  let elementWidth = element.offsetWidth;
-  let parentWidth = element.parentElement.offsetWidth;
-  let flag = 0;
-
-  setInterval(() => {
-    if (flag >= parentWidth) {
-      flag = -elementWidth;
-    }
-    element.style.marginLeft = flag + "px";
-    flag++;
-  }, 10);
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
 }
